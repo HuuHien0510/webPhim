a4a83e1e9563390d6fe6c139f2875a55395d5029
HuuHien0510 <huhhn05102@gmai.com>
2024-10-27 21:16:57 +0700

    Add project files.

---
+++ webPhim/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webPhim.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult TrangChu()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu2()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu3()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu4()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu5()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu6()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ webPhim/Scripts/StartLightBMTJavaScript.js
@@ -0,0 +1,32 @@
+﻿// script.js
+
+const showtimesData = {
+    "Venom": {
+        "24/10/2024": ["19:10", "21:00", "23:30"],
+        "25/10/2024": ["18:00", "20:30", "22:30"]
+    },
+    "Cô Dâu Hào Môn": {
+        "24/10/2024": ["11:40", "15:05", "19:00"],
+        "25/10/2024": ["10:30", "14:00", "16:30"]
+    }
+};
+
+// Hàm để cập nhật giờ chiếu phim
+function updateShowtimes(movieTitle, date) {
+    const movie = showtimesData[movieTitle];
+    const times = movie[date] || [];
+    const showtimesDiv = document.querySelector('.movie-info .showtimes');
+
+    // Xóa các nút giờ chiếu hiện tại
+    showtimesDiv.innerHTML = '';
+
+    // Thêm giờ chiếu mới
+    times.forEach(time => {
+        const button = document.createElement('button');
+        button.textContent = time;
+        showtimesDiv.appendChild(button);
+    });
+}
+
+// Gọi hàm này khi người dùng chọn ngày
+updateShowtimes("Venom", "24/10/2024");
